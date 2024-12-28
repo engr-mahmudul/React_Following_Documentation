72ac7e61fdebbf127d3368d2d90e7e6da6f5f257
engr-mahmudul <dev.mahmudulhasan@gmail.com>
2024-12-29 00:11:41 +0200

    Borad turn funcationalities done

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -1,17 +1,43 @@
-function Square({ value }) {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+function Square({ value, setSquaresValue }) {
   return (
-    <button className="background-white border border-gray-300 h-12 w-12 m-1 text-lg leading-9">
+    <button
+      className="background-white border border-gray-300 h-12 w-12 m-1 text-lg leading-9"
+      onClick={() => setSquaresValue()}
+    >
       {value}
     </button>
   );
 }
 
 export default function Board() {
-  const a = Array.from({ length: 9 }, (_, i) => i + 1);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const a = Array.from({ length: 9 }, (_, i) => i);
+
+  const handleSquareClick = (index) => {
+    if (squares[index]) {
+      return;
+    }
+    const newSquares = [...squares];
+    if (xIsNext) {
+      newSquares[index] = "X";
+    } else {
+      newSquares[index] = "O";
+    }
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
   return (
     <div className="grid grid-cols-3 gap-0 w-40">
       {a.map((i) => (
-        <Square value={i} key={i} />
+        <Square
+          value={squares[i]}
+          key={i}
+          setSquaresValue={() => handleSquareClick(i)}
+        />
       ))}
     </div>
   );
