b6b35d321116d92bfb3dc7202107f058df2bd6ed
engr-mahmudul <dev.mahmudulhasan@gmail.com>
2025-01-06 17:48:18 +0200

    Understood the snapshot, batch updating and updating function

--- adding-interactivity/src/components/UseStateHook.jsx
+++ adding-interactivity/src/components/UseStateHook.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 export default function UseStateHook() {
   const [counter, setCounter] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [batchUpdater, setBatchUpdater] = useState(0);
+  const [updater, setUpdater] = useState(0);
+
+  // console.log(counter);
+  // console.log(open);
   return (
     <>
       <p>{counter}</p>
+
       <button onClick={() => setCounter(counter + 1)}>
         Click here to increase the number
       </button>
@@ -13,6 +20,40 @@ export default function UseStateHook() {
       >
         Click here to decrease the number
       </button>
+      <p style={{ color: open ? "green" : "red" }}>
+        {open ? "Second State is Open" : "Second State is Closed"}
+      </p>
+      <button onClick={() => setOpen(!open)}>Handle Second Sate</button>
+      <div style={{ display: "flex" }}>
+        <div
+          style={{ margin: "20px", border: "3px solid black", padding: "10px" }}
+        >
+          <p>{batchUpdater}</p>
+          <button
+            onClick={() => {
+              setBatchUpdater(batchUpdater + 1);
+              setBatchUpdater(batchUpdater + 1);
+              setBatchUpdater(batchUpdater + 1);
+            }}
+          >
+            Increase with Updater
+          </button>
+        </div>
+        <div
+          style={{ margin: "20px", border: "3px solid black", padding: "10px" }}
+        >
+          <p>{updater}</p>
+          <button
+            onClick={() => {
+              setUpdater((x) => x + 1);
+              setUpdater((x) => x + 1);
+              setUpdater((x) => x + 1);
+            }}
+          >
+            Increase with Updater Function
+          </button>
+        </div>
+      </div>
     </>
   );
 }
