f26b276edb78c2685af51ee5323581ee30259b9d
engr-mahmudul <dev.mahmudulhasan@gmail.com>
2025-01-29 23:21:54 +0200

    nested routing

--- react-router-dom/src/ContactPage.jsx
+++ react-router-dom/src/ContactPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Form } from "react-router-dom";
 
 export default function Contact() {
--- react-router-dom/src/main.jsx
+++ react-router-dom/src/main.jsx
@@ -12,10 +12,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
